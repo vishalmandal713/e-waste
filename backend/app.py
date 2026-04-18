e7d7036856611d3e5de9db0a8abde3c89a3a3c20
Vishal Mandal <vishalji713@gmail.com>
2026-04-19 01:43:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-```python
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -19,4 +19,4 @@ def get_requests():
 
 if __name__ == '__main__':
     app.run(debug=True)
-```
+
